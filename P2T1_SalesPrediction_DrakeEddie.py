fed6963fc75d23cf16f674b861b0e68b19633b39
drakee7951 <36830557+drakee7951@users.noreply.github.com>
2018-02-25 22:07:24 -0500

    Add files via upload
    Following instructions from lab.

---
+++ P2T1_SalesPrediction_DrakeEddie.py
@@ -0,0 +1,7 @@
+# CTI-110
+# P2T1-Sales Prdiction
+# DRAKE
+# 22 Feb 2018
+total_sales = float (input("Enter the projected sales: "))
+profit = total_sales * 0.23
+print('The profit is $', format(profit, ',.2f'))
